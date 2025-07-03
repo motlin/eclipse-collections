197b0fbcf9a0c6fd6716acefcc97de47f3cdb91a
Craig P. Motlin <cmotlin@gmail.com>
2025-07-03 16:00:55 -0400

    Create ImmutableUnmodifiableCollection bridge interface.

---
+++ eclipse-collections/src/main/java/org/eclipse/collections/impl/collection/immutable/ImmutableUnmodifiableCollection.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2025 Goldman Sachs and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package org.eclipse.collections.impl.collection.immutable;
+
+import org.eclipse.collections.api.collection.ImmutableCollection;
+import org.eclipse.collections.impl.ImmutableUnmodifiableIterable;
+
+/**
+ * ImmutableUnmodifiableCollection is a marker interface that indicates a collection is both immutable
+ * and can serve as an unmodifiable view of a mutable collection.
+ * 
+ * This interface does NOT extend UnmodifiableMutableCollection directly due to return type conflicts.
+ * Instead, concrete implementations should:
+ * 1. Extend their respective immutable base class (e.g., AbstractImmutableCollection)
+ * 2. Implement this marker interface
+ * 3. Provide implementations of mutable methods that throw UnsupportedOperationException
+ * 
+ * The purpose is to allow immutable collections to be used where UnmodifiableMutableCollection types are expected,
+ * without causing compilation errors due to incompatible return types between ImmutableCollection and MutableCollection.
+ * 
+ * @since 12.0
+ */
+public interface ImmutableUnmodifiableCollection<T> extends ImmutableUnmodifiableIterable<T>, ImmutableCollection<T>
+{
+}
