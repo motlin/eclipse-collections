6bfb4a4895b6107c72b56b5abf0eb6926d8d40a0
Craig P. Motlin <cmotlin@gmail.com>
2025-10-11 19:57:05 +0000

    Add TreeMapTest for java.util.TreeMap test coverage.

---
+++ unit-tests-java8/src/test/java/org/eclipse/collections/test/map/mutable/sorted/TreeMapTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2024 Goldman Sachs and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package org.eclipse.collections.test.map.mutable.sorted;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.eclipse.collections.impl.block.factory.Comparators;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TreeMapTest implements SortedMapTestCase
+{
+    @Override
+    public <T> SortedMap<Object, T> newWith(T... elements)
+    {
+        int i = elements.length;
+        SortedMap<Object, T> result = new TreeMap<>(Comparators.reverseNaturalOrder());
+        for (T each : elements)
+        {
+            assertNull(result.put(i, each));
+            i--;
+        }
+        return result;
+    }
+
+    @Override
+    public <K, V> SortedMap<K, V> newWithKeysValues(Object... elements)
+    {
+        if (elements.length % 2 != 0)
+        {
+            fail(String.valueOf(elements.length));
+        }
+
+        SortedMap<K, V> result = new TreeMap<>(Comparators.reverseNaturalOrder());
+        for (int i = 0; i < elements.length; i += 2)
+        {
+            assertNull(result.put((K) elements[i], (V) elements[i + 1]));
+        }
+        return result;
+    }
+}
