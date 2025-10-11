0058d598d3bc988c843ebf114d2edc820b65faf7
Craig P. Motlin <cmotlin@gmail.com>
2025-10-11 17:51:17 +0000

    Add TreeSetTest for test coverage.

---
+++ unit-tests-java8/src/test/java/org/eclipse/collections/test/set/mutable/sorted/TreeSetTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2022 Goldman Sachs and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package org.eclipse.collections.test.set.mutable.sorted;
+
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.eclipse.collections.test.set.sorted.SortedSetTestCase;
+import org.junit.jupiter.api.Test;
+
+import static org.eclipse.collections.impl.test.Verify.assertPostSerializedEqualsAndHashCode;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class TreeSetTest
+        implements SortedSetTestCase
+{
+    @SafeVarargs
+    @Override
+    public final <T> SortedSet<T> newWith(T... elements)
+    {
+        SortedSet<T> result = new TreeSet<>(Collections.reverseOrder());
+        Collections.addAll(result, elements);
+        return result;
+    }
+
+    @Override
+    public boolean allowsDuplicates()
+    {
+        return false;
+    }
+
+    @Override
+    @Test
+    public void Object_equalsAndHashCode()
+    {
+        assertPostSerializedEqualsAndHashCode(this.newWith(3, 2, 1));
+
+        assertNotEquals(this.newWith(4, 3, 2, 1), this.newWith(3, 2, 1));
+        assertNotEquals(this.newWith(3, 2, 1), this.newWith(4, 3, 2, 1));
+
+        assertNotEquals(this.newWith(2, 1), this.newWith(3, 2, 1));
+        assertNotEquals(this.newWith(3, 2, 1), this.newWith(2, 1));
+
+        assertNotEquals(this.newWith(4, 2, 1), this.newWith(3, 2, 1));
+        assertNotEquals(this.newWith(3, 2, 1), this.newWith(4, 2, 1));
+    }
+
+    @Override
+    @Test
+    public void Iterable_toString()
+    {
+        assertEquals("[3, 2, 1]", this.newWith(3, 2, 1).toString());
+    }
+}
