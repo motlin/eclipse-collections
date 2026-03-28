56b6488dfa5dc5b7a3cda03136d23cde1eb250c2
Craig P. Motlin <cmotlin@gmail.com>
2026-03-28 15:48:15 +0000

    Use static imports for Assertions in EmptyLazyIterableTest.

--- unit-tests/src/test/java/org/eclipse/collections/impl/lazy/EmptyLazyIterableTest.java
+++ unit-tests/src/test/java/org/eclipse/collections/impl/lazy/EmptyLazyIterableTest.java
@@ -12,8 +12,13 @@ package org.eclipse.collections.impl.lazy;
 
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.impl.JolMemoryTestUtil;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EmptyLazyIterableTest
 {
@@ -27,7 +32,7 @@ public class EmptyLazyIterableTest
     @Test
     public void instance()
     {
-        Assertions.assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance());
+        assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance());
     }
 
     @Test
@@ -39,139 +44,139 @@ public class EmptyLazyIterableTest
     @Test
     public void into()
     {
-        Assertions.assertEquals(Lists.immutable.with(1), EmptyLazyIterable.getInstance().into(Lists.mutable.with(1)));
+        assertEquals(Lists.immutable.with(1), EmptyLazyIterable.getInstance().into(Lists.mutable.with(1)));
     }
 
     @Test
     public void size()
     {
-        Assertions.assertEquals(0, EmptyLazyIterable.getInstance().size());
+        assertEquals(0, EmptyLazyIterable.getInstance().size());
     }
 
     @Test
     public void isEmpty()
     {
-        Assertions.assertTrue(EmptyLazyIterable.getInstance().isEmpty());
+        assertTrue(EmptyLazyIterable.getInstance().isEmpty());
     }
 
     @Test
     public void notEmpty()
     {
-        Assertions.assertFalse(EmptyLazyIterable.getInstance().notEmpty());
+        assertFalse(EmptyLazyIterable.getInstance().notEmpty());
     }
 
     @Test
     public void select()
     {
-        Assertions.assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().select(e -> true));
+        assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().select(e -> true));
     }
 
     @Test
     public void reject()
     {
-        Assertions.assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().reject(e -> true));
+        assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().reject(e -> true));
     }
 
     @Test
     public void drop()
     {
-        Assertions.assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().drop(1));
+        assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().drop(1));
     }
 
     @Test
     public void take()
     {
-        Assertions.assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().take(1));
+        assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().take(1));
     }
 
     @Test
     public void takeWhile()
     {
-        Assertions.assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().takeWhile(e -> true));
+        assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().takeWhile(e -> true));
     }
 
     @Test
     public void dropWhile()
     {
-        Assertions.assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().dropWhile(e -> true));
+        assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().dropWhile(e -> true));
     }
 
     @Test
     public void distinct()
     {
-        Assertions.assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().distinct());
+        assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().distinct());
     }
 
     @Test
     public void detect()
     {
-        Assertions.assertNull(EmptyLazyIterable.getInstance().detect(e -> true));
+        assertNull(EmptyLazyIterable.getInstance().detect(e -> true));
     }
 
     @Test
     public void detectWith()
     {
-        Assertions.assertNull(EmptyLazyIterable.getInstance().detectWith((e, f) -> true, null));
+        assertNull(EmptyLazyIterable.getInstance().detectWith((e, f) -> true, null));
     }
 
     @Test
     public void detectOptional()
     {
-        Assertions.assertTrue(EmptyLazyIterable.getInstance().detectOptional(e -> true).isEmpty());
+        assertTrue(EmptyLazyIterable.getInstance().detectOptional(e -> true).isEmpty());
     }
 
     @Test
     public void detectWithOptional()
     {
-        Assertions.assertTrue(EmptyLazyIterable.getInstance().detectWithOptional((e, f) -> true, null).isEmpty());
+        assertTrue(EmptyLazyIterable.getInstance().detectWithOptional((e, f) -> true, null).isEmpty());
     }
 
     @Test
     public void anySatisfy()
     {
-        Assertions.assertFalse(EmptyLazyIterable.getInstance().anySatisfy(e -> true));
+        assertFalse(EmptyLazyIterable.getInstance().anySatisfy(e -> true));
     }
 
     @Test
     public void anySatisyWith()
     {
-        Assertions.assertFalse(EmptyLazyIterable.getInstance().anySatisfyWith((e, f) -> true, null));
+        assertFalse(EmptyLazyIterable.getInstance().anySatisfyWith((e, f) -> true, null));
     }
 
     @Test
     public void allSatisfy()
     {
-        Assertions.assertTrue(EmptyLazyIterable.getInstance().allSatisfy(e -> true));
+        assertTrue(EmptyLazyIterable.getInstance().allSatisfy(e -> true));
     }
 
     @Test
     public void allSatisyWith()
     {
-        Assertions.assertTrue(EmptyLazyIterable.getInstance().allSatisfyWith((e, f) -> true, null));
+        assertTrue(EmptyLazyIterable.getInstance().allSatisfyWith((e, f) -> true, null));
     }
 
     @Test
     public void noneSatisfy()
     {
-        Assertions.assertTrue(EmptyLazyIterable.getInstance().noneSatisfy(e -> true));
+        assertTrue(EmptyLazyIterable.getInstance().noneSatisfy(e -> true));
     }
 
     @Test
     public void noneSatisyWith()
     {
-        Assertions.assertTrue(EmptyLazyIterable.getInstance().noneSatisfyWith((e, f) -> true, null));
+        assertTrue(EmptyLazyIterable.getInstance().noneSatisfyWith((e, f) -> true, null));
     }
 
     @Test
     public void collect()
     {
-        Assertions.assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().collect(e -> null));
+        assertSame(EmptyLazyIterable.getInstance(), EmptyLazyIterable.getInstance().collect(e -> null));
     }
 
     @Test
     public void collectIf()
     {
-        Assertions.assertSame(
+        assertSame(
                 EmptyLazyIterable.getInstance(),
                 EmptyLazyIterable.getInstance().collectIf(e -> true, e -> null));
     }
