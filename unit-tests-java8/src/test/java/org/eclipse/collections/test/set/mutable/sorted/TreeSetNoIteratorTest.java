ed452f4f5aa46a52cd49d4bac70319435233d46e
Craig P. Motlin <cmotlin@gmail.com>
2026-03-31 15:46:42 +0000

    Add NavigableSetNoIteratorTestCase and NoIterator tests for TreeSet.

---
+++ unit-tests-java8/src/test/java/org/eclipse/collections/test/set/mutable/sorted/TreeSetNoIteratorTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026 Goldman Sachs and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package org.eclipse.collections.test.set.mutable.sorted;
+
+import java.util.Collections;
+import java.util.NavigableSet;
+
+import org.eclipse.collections.test.set.sorted.NavigableSetNoIteratorTestCase;
+
+public class TreeSetNoIteratorTest implements NavigableSetNoIteratorTestCase
+{
+    @SafeVarargs
+    @Override
+    public final <T> NavigableSet<T> newWith(T... elements)
+    {
+        NavigableSet<T> result = new TreeSetNoIterator<>(Collections.reverseOrder());
+        Collections.addAll(result, elements);
+        return result;
+    }
+
+    @Override
+    public boolean allowsDuplicates()
+    {
+        return false;
+    }
+}
